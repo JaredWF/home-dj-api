be9c409c5b14353b56bbed408c8af67b46ef434f
Jared Failor <jfailor@cfu.net>
2016-01-31 16:40:47 -0500

    Change content type on ajax response

--- src/main/resources/djClient.js
+++ src/main/resources/djClient.js
@@ -43,7 +43,7 @@ window.onload = function () {
                   data: JSON.stringify(
                       {id: uri}
                   ),
-                  contentType: "application/json",
+                  contentType: "text/plain",
                   dataType   : "text/plain",
                   success: function(response){  
                     alert("successful post");
