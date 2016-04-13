6982c6f9f7998bdf5c7de620d081b1ba0039a119
Jared Failor <jfailor@cfu.net>
2016-04-12 21:25:02 -0400

    Add front end code for playlist retrieval

--- src/main/resources/js/owner.js
+++ src/main/resources/js/owner.js
@@ -10,28 +10,67 @@ window.onload = function () {
   console.log("code = \n" + code);
   console.log("domain = " + domain);
 
+  loginAndShowPlaylists(code, domain);
+};
 
+function loginAndShowPlaylists(code, domain) {
   $.ajax({  
       type: "GET",  
       dataType: "text",
       url: domain + "/finishAuthorize?code=" + code,  
       success: function(hash){
         console.log("hash = " + hash);
-
-        $.ajax({  
-          type: "GET",  
-          dataType: "json",
-          url: domain + "/getPlaylists/" + hash,  
-          success: function(json){
-            console.log(json);
-          },
-          error: function(e){  
-            //alert("failure: \n" + e);  
-          }
-        });
+        var playlists = getPlaylists(hash, domain);
       },
       error: function(e){  
         //alert("failure: \n" + e);  
       }
   });
-};
+}
+
+function getPlaylists(hash, domain) {
+  $.ajax({  
+    type: "GET",  
+    dataType: "json",
+    url: domain + "/getPlaylists/" + hash,  
+    success: function(json){
+      console.log(json);
+      displayPlaylists(json);
+    },
+    error: function(e){  
+      //alert("failure: \n" + e);  
+    }
+  });
+}
+
+function displayPlaylists(json) {
+  $(".loader").remove();
+
+  var p = document.createElement('p');
+  $(p).html("Choose a playlist")
+  .appendTo($("#body"));
+
+
+  for(var i = 0; i < json.length; i++) {
+    var playlist = json[i];
+
+    var d = document.createElement('div');
+    $(d).addClass("playlistCard")
+    .appendTo($("#body"));
+
+    var thumbnail = document.createElement('img');
+    $(thumbnail).addClass("albumImage")
+    .attr('src', playlist.imageURL)
+    .appendTo(d);
+
+    var title = document.createElement('div');
+    $(title).addClass("playlistTitle")
+    .html(playlist.name)
+    .appendTo(d);
+
+    var songCount = document.createElement('div');
+    $(songCount).addClass("playlistLength")
+    .html(playlist.trackCount + " Songs")
+    .appendTo(d);
+  }
+}
